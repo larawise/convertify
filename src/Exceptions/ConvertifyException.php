5319dc2b822c359ff91efc3337cd231447698436
Selçuk Çukur <5716652+selcukcukur@users.noreply.github.com>
2025-10-10 12:00:10 +0300

    A special exception class for runtime errors.

---
+++ src/Exceptions/ConvertifyException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Larawise\Convertify\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Convertify
+ * @version     v1.0.0
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+class ConvertifyException extends RuntimeException
+{
+    // ...
+}
