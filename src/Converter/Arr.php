b197e62ebe1d0e88b07c346ff1bf3367efdca1f3
selcukcukur <selcukcukur@outlook.com.tr>
2025-10-10 13:38:38 +0300

    `Array` converter support for `Convertify`

---
+++ src/Converter/Arr.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Larawise\Convertify\Converter;
+
+use Larawise\Convertify\Contracts\ConverterContract;
+
+/**
+ * Srylius - The ultimate symphony for technology architecture!
+ *
+ * @package     Larawise
+ * @subpackage  Convertify
+ * @version     v1.0.0
+ * @author      Selçuk Çukur <hk@selcukcukur.com.tr>
+ * @copyright   Srylius Teknoloji Limited Şirketi
+ *
+ * @see https://docs.larawise.com/ Larawise : Docs
+ */
+class Arr implements ConverterContract
+{
+    /**
+     * Determine whether this converter should handle casting for the given key and value.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function shouldCast(string $key, $value)
+    {
+        return is_string($value) && str_contains($value, ',');
+    }
+
+    /**
+     * Convert a raw external value into its appropriate PHP-native type.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function cast(string $key, $value)
+    {
+        return array_map('trim', explode(',', $value));
+    }
+
+    /**
+     * Determine whether this converter should handle uncasting for the given key and value.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function shouldUncast(string $key, $value)
+    {
+        return is_array($value);
+    }
+
+    /**
+     * Convert a PHP-native value into a storable format (e.g. string, JSON).
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    public function uncast(string $key, $value)
+    {
+        return implode(',', $value);
+    }
+}
